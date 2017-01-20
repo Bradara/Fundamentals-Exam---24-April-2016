edfe5391e94f7009d9669d8b75f8a6ca41ae3c70
Atanas Nikolov <atanas.nikolov74@gmail.com>
2017-01-20 09:03:15 +0200

    Added Problem 3 Strawberry
    Programming for Beginners – 24 April 2016
    Problem 3. Strawberry
    Your task is to write a program, which reads a number N from the console
    and draws a strawberry, which size depends on N. See the examples below
    to understand your task better.
    Input
    On the only input line you will be given the positive number N – odd
    number in the range [1 … 17]
    Output
    The output should be printed on the console. Use the “#” (number sign)
    for the outlines of the strawberry, “.” for the inside of the
    strawberry. For the leaves:  left ones – “\”, middle ones – “|” and
    right ones – “/”
    Examples
    Input Output Input Output Input Output
    5 \-----|-----/
    --\---|---/--
    -----#.#-----
    ---#.....#---
    -#.........#-
    #...........#
    -#.........#-
    --#.......#--
    ---#.....#---
    ----#...#----
    -----#.#----- 7 \-------|-------/
    --\-----|-----/--
    ----\---|---/----
    -------#.#-------
    -----#.....#-----
    ---#.........#---
    -#.............#-
    #...............#
    -#.............#-
    --#...........#--
    ---#.........#---
    ----#.......#----
    -----#.....#-----
    ------#...#------
    -------#.#------- 9 \---------|---------/
    --\-------|-------/--
    ----\-----|-----/----
    ------\---|---/------
    ---------#.#---------
    -------#.....#-------
    -----#.........#-----
    ---#.............#---
    -#.................#-
    #...................#
    -#.................#-
    --#...............#--
    ---#.............#---
    ----#...........#----
    -----#.........#-----
    ------#.......#------
    -------#.....#-------
    --------#...#--------
    ---------#.#---------

---
+++ 3.Strawberry/.Strawberry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Strawberry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n/2; i++)
+            {
+                Console.WriteLine("{0}\\{1}|{1}/{0}", new string('-', i*2), new string('-', n-2*i));
+            }
+            for (int i = n; i > 0; i-=2)
+            {
+                Console.WriteLine("{0}#{1}#{0}", new string('-', i), new string('.', 2*n+1-2*i));
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("{1}#{0}#{1}", new string('.', 2 * n + 1-2*i), new string('-', i));
+            }
+            
+        }
+    }
+}
